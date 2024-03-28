3e2343df930ebcc1f519b45b88ffd383df46abf5
Sabuj Bandopadhyay <68777030+sabuj000@users.noreply.github.com>
2024-03-28 17:33:02 +0530

    Delete .next directory

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/interactive-js":["static\u002Fchunks\u002Fpages\u002Finteractive-js.js"],sortedPages:["\u002F_app","\u002F_error","\u002Finteractive-js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
