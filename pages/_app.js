16a5576520f1f3f1896523696cc36bb710b4c2bc
Sabuj Bandopadhyay <68777030+sabuj000@users.noreply.github.com>
2025-04-15 19:11:00 +0530

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+// pages/_app.js
+import { MDXProvider } from '@mdx-js/react'
+import ApiRequest from '../components/ApiRequest'
+import ApiResponse from '../components/ApiResponse'
+import '../styles/globals.css'
+
+const components = {
+  ApiRequest,
+  ApiResponse,
+  // Add more custom components here
+}
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <MDXProvider components={components}>
+      <Component {...pageProps} />
+    </MDXProvider>
+  )
+}
