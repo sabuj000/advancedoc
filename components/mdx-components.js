7e2e8568744c1cd7d11654892397fb1ace222ede
Sabuj Bandopadhyay <68777030+sabuj000@users.noreply.github.com>
2025-04-15 20:39:05 +0530

    Create mdx-components.js

---
+++ components/mdx-components.js
@@ -0,0 +1,11 @@
+// components/mdx-components.js
+import ApiRequest from './ApiRequest'
+import ApiResponse from './ApiResponse'
+
+const components = {
+  ApiRequest,
+  ApiResponse,
+  // Add more custom components here
+}
+
+export default components
